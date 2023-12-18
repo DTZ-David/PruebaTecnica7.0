5ed893f3c65c21e061d569cbd622d90b2b7ab121
DTZ-David <79215503+DTZ-David@users.noreply.github.com>
2023-12-18 09:30:02 -0500

    Docker with mongo

--- PruebaTecnica7.0/Controllers/ProveedorController.cs
+++ PruebaTecnica7.0/Controllers/ProveedorController.cs
@@ -11,7 +11,7 @@ namespace PruebaTecnica.Controllers
 
     [Route("api/[controller]")]
     [ApiController]
-    
+    [Authorize]
     public class ProveedorController : ControllerBase
     {
         private readonly IProveedorService proveedorService;
